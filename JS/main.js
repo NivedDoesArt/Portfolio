4ba75384c0b01c7ce3e3378e34e72c6a45f536a4
NivedDoesArt <devindewit2004@gmail.com>
2024-11-27 20:38:27 +0100

    veel

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,8 @@
-console.log('hey there! :D')
+addEventListener("scroll", (event) => {
+    var distanceScrolled = window.scrollY
+    document.getElementById('backgroundDevin').style.transform = 'scale(' + (100 + (distanceScrolled / 80)) + '%) translateY(-' + (distanceScrolled / 3) + 'px)'
+});
+
+window.onbeforeunload = function() {
+    window.scrollTo(0,0)
+}
