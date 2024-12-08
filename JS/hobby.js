342dca73f5988685dd12d2f35501ca8f177d7b13
NivedDoesArt <devindewit2004@gmail.com>
2024-12-08 22:16:55 +0100

    8-12-2024 1

--- JS/hobby.js
+++ JS/hobby.js
@@ -15,4 +15,27 @@ if (window.innerWidth < 1000) {
     document.getElementById('header').children[0].remove()
     document.getElementById('header').children[0].remove()
     document.getElementById('header').style.height = '50px'
+
+    document.getElementById('footerSocials').style.width = 'calc((100% / 2) - 11%)'
+    document.getElementById('footerLinks').style.width = 'calc((100% / 2) - 11%)'
+    document.getElementById('footerContact').style.width = 'calc(100% - 11%)'
+
+    for(i = 1; i <= 7; i++) {
+        document.getElementById('title' + i).style.textAlign = 'center'
+        document.getElementById('title' + i).style.fontSize = '8vw'
+        document.getElementById('title' + i).style.paddingLeft = '0px'
+
+        document.getElementById('description' + i).style.width = 'calc(100% - (9.5vw * 2))'
+        document.getElementById('description' + i).style.textAlign = 'justify'
+        document.getElementById('description' + i).style.paddingLeft = '9.5vw'
+        document.getElementById('description' + i).style.paddingRight = '9.5vw'
+    }
+}
+
+
+
+
+
+function imageDisplay(location) {
+    console.log(location)
 }
